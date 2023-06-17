beba4084ffbd4e29e908d1551aa2d08599a33ea0
22041398-ffmeng <fadedzipper@163.com>
2023-06-17 16:40:56 +0800

    2023年 06月 17日 星期六 16:40:56 CST

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 cp ~/.ideavimrc ./
 cp ~/.zshrc ./
-echo "have cp ideavimrc and zshrc to this directory"
+echo "INFO: have cp ideavimrc and zshrc to this directory"
 git add .
 git commit -m "`date`"
 git push origin main
-echo "upload to github repo"
+echo "INFO: upload to github repo"
