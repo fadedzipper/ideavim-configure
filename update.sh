cbe087404ab0cb581e5d963aaf619e4f58bbc4e3
22041398-ffmeng <fadedzipper@163.com>
2023-06-17 16:45:29 +0800

    2023年 06月 17日 星期六 16:45:29 CST

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
+echo "INFO: cp ideavimrc and zshrc to this directory"
 cp ~/.ideavimrc ./
 cp ~/.zshrc ./
-echo "INFO: have cp ideavimrc and zshrc to this directory"
+cp ~/.config/Code/User/settings.json ~/.config/Code/User/keybindings.json ./
+echo "INFO: upload to github repo"
 git add .
 git commit -m "`date`"
 git push origin main
-echo "INFO: upload to github repo"
